f3641505307b75c2532c6acef16307c239dec7a7
gkpk30 <46205709+gkpk30@users.noreply.github.com>
2022-05-07 13:42:41 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./index.html", "./src/**/*.jsx"],
   darkMode: "media", // 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+       
+        'sans': ['Josefin', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'background-beige': '#243c5a',
+        'secondary': '#54595F',
+        'third' : '#3f4347',
+        'text': '#7A7A7A',
+      },
+    },
   },
   variants: {
     extend: {},
